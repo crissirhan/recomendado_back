d7164d9d56590354fd33f909393df551da54ed00
Samirovich <samiruli@gmail.com>
2017-09-08 13:02:22 -0300

    admin panel y mas

--- users/admin.py
+++ users/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from users.models import *
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Professional)
+admin.site.register(Review)
--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -11,6 +12,9 @@ class User(models.Model):
 
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return self.name
 
 class Client(User):
     pass
@@ -22,3 +26,8 @@ class Review(models.Model):
     client = models.ForeignKey('Client')
     professional = models.ForeignKey('Professional')
     rating = models.IntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    comment = models.CharField(max_length=10000, null=True)
+    date = models.DateTimeField(null=False)
+
+    def __str__(self):
+        return 'Review de: ' + self.client.name + ' a: ' + self.professional.name
