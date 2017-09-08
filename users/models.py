8070c4de83957e5ae7229402da0c61b496f8e975
Samirovich <samiruli@gmail.com>
2017-09-08 11:49:22 -0300

    empezando modelo

--- users/models.py
+++ users/models.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+class Client(User):
+    pass
+
+class Professional(User):
+    profession = models.CharField(max_length=100)
