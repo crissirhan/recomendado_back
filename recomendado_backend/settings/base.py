91232a4fa39e28c331e0a18daaa9ce1f29cd0c8c
Samirovich <samiruli@gmail.com>
2017-09-09 14:20:35 -0300

    multiple choices

--- recomendado_backend/settings/base.py
+++ recomendado_backend/settings/base.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'rest_framework',
+    'users',
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.contrib.auth.models import User
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 
@@ -22,7 +23,7 @@ class Professional(ExtendedUser):
     house_number = models.IntegerField()
     phone_regex = RegexValidator(regex=r'^\+?1?\d{8,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(max_length = 15, validators=[phone_regex], blank=True)
-    identification = models.ImageField(upload_to='', null=True)
+    identification = models.ImageField(upload_to='images/', null=True)
 
 class Review(models.Model):
     client = models.ForeignKey('Client')
@@ -35,12 +36,20 @@ class Review(models.Model):
         return 'Review from: ' + self.client.name + ' to: ' + self.professional.name + '. Date: ' + self.date.strftime(" %d %B, %Y")
 
 class Announcement(models.Model):
+    WEEKDAYS = (('mon', 'Monday'),
+              ('tue', 'Tuesday'),
+              ('wed', 'Wednesday'),
+              ('thurs', 'Thursday'),
+              ('frid', 'Friday'),
+              ('sat', 'Saturday'),
+              ('sun', 'Sunday'))
+
     professional = models.ForeignKey('Professional')
     publish_date = models.DateTimeField(null=False)
     expire_date = models.DateTimeField(null=False)
     job = models.CharField(max_length=20)
     location = models.CharField(max_length=50)
-    availability = models.CharField(max_length=100)
+    availability = MultiSelectField(choices=WEEKDAYS, max_choices=7)
     movility = models.CharField(max_length=50)
 
     def __str__(self):
