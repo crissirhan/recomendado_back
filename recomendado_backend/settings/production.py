dd15362f3551d422726da5478cd24d077ccce552
Samirovich <samiruli@gmail.com>
2017-09-12 18:39:26 -0300

    ajustes de produccion

--- recomendado_backend/settings/production.py
+++ recomendado_backend/settings/production.py
@@ -1,3 +1,15 @@
 from recomendado_backend.settings.base import *
 
 DEBUG = False
+ALLOWED_HOSTS = ['35.196.31.174']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'GeoYingoDB-Dev',
+        'HOST': '35.185.125.245',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'ckItdb01MZF3'
+    }
+}
