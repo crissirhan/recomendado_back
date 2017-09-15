0873ecd780e32b47f3b284cc5ede967937716993
Samirovich <samiruli@gmail.com>
2017-09-15 14:35:05 -0300

    removido como campo necesario la imagen en profesionales

--- users/models.py
+++ users/models.py
@@ -23,7 +23,7 @@ class Professional(ExtendedUser):
     house_number = models.IntegerField()
     phone_regex = RegexValidator(regex=r'^\+?1?\d{8,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(max_length = 15, validators=[phone_regex], blank=True)
-    identification = models.ImageField(upload_to='images/', null=True)
+    identification = models.ImageField(upload_to='images/', blank=True, null=True)
 
 class Review(models.Model):
     client = models.ForeignKey('Client')
