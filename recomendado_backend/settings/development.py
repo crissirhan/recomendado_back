4c27406d095993214b08a23318bc16009ff57c5c
Samir Saleh <samiruli@gmail.com>
2017-10-19 15:10:05 -0300

    media url

--- recomendado_backend/settings/development.py
+++ recomendado_backend/settings/development.py
@@ -4,6 +4,7 @@ DEBUG = True
 
 # Here in the OS the images and files witll be uploaded and served
 MEDIA_ROOT = '/var/www/recomendado/'
+MEDIA_URL = os.path.join(BASE_DIR,'/media/')
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
