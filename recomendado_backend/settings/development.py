606582c8cf4b789b5864bab52326fbec6c239f41
Samir Saleh <samiruli@gmail.com>
2017-10-19 14:52:27 -0300

    media_root para deployemnet

--- recomendado_backend/settings/development.py
+++ recomendado_backend/settings/development.py
@@ -2,6 +2,8 @@ from recomendado_backend.settings.base import *
 
 DEBUG = True
 
+# Here in the OS the images and files witll be uploaded and served
+MEDIA_ROOT = '/var/www/recomendado/'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
