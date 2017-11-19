bfa3178869f10c92d39efca3f0be8c3b05c5bab4
Samir Saleh <samiruli@gmail.com>
2017-11-19 01:10:25 -0300

    elementos x pagina

--- users/pagination.py
+++ users/pagination.py
@@ -11,9 +11,14 @@ class StandardResultsSetPagination(PageNumberPagination):
         return Response(OrderedDict([
              ('lastPage', self.page.paginator.num_pages),
              ('totalElements', self.page.paginator.count),
-             ('countItemsOnPage', self.page_size),
+             ('countItemsOnPage', self.get_page_size()),
              ('current', self.page.number),
              ('next', self.get_next_link()),
              ('previous', self.get_previous_link()),
              ('results', data)
          ]))
+
+    def get_page_size(self):
+        if self.page_size_query_param:
+            return self.page_size_query_param
+        return self.page_size
