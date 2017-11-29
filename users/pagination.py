405f4a94c08f267dde58d3c4168e1f86346473fe
Samir Saleh <samiruli@gmail.com>
2017-11-28 23:42:42 -0300

    paginator per page)

--- users/pagination.py
+++ users/pagination.py
@@ -15,5 +15,6 @@ class StandardResultsSetPagination(PageNumberPagination):
              ('current', self.page.number),
              ('next', self.get_next_link()),
              ('previous', self.get_previous_link()),
-             ('results', data)
+             ('page_size', self.page.paginator.per_page),
+             ('results', data),
          ]))
