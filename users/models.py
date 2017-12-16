95d1458d830a97476a3c55e25239244d7ba0256d
Samir Saleh <samiruli@gmail.com>
2017-12-16 01:24:03 -0300

    fix related name

--- users/models.py
+++ users/models.py
@@ -109,7 +109,7 @@ class JobSubCategory(models.Model):
 
 class Service(models.Model):
     announcement = models.ForeignKey('Announcement',related_name='service')
-    client = models.ForeignKey('Client',related_name='service')
+    client = models.ForeignKey('Client',related_name='client')
     cost = MoneyField(max_digits=10, decimal_places=0, default_currency='CLP', null=True)
     creation_date = models.DateTimeField(null=False)
     contacted_date = models.DateTimeField(null=True, blank=True)
--- users/views.py
+++ users/views.py
@@ -241,7 +241,7 @@ class JobSubCategoryViewSet(viewsets.ModelViewSet):
     serializer_class = JobSubCategoriesSerializer
 
 class ServiceViewSet(viewsets.ModelViewSet):
-    queryset = Service.objects.filter(deleted=False).annotate(review_average = Avg('review__rating')).annotate(review_count = Count('review'))
+    queryset = Service.objects.annotate(review_average = Avg('review__rating')).annotate(review_count = Count('review')).filter(deleted=False)
     serializer_class = ServicesSerializer
     filter_backends = (filters.DjangoFilterBackend, OrderingFilter,)
     filter_class = ServiceFilter
