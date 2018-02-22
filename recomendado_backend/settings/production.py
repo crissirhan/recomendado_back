652964b7a817b21a6cea0a0b4c9444ada21e54b0
Samirovich <Samirovich@users.noreply.github.com>
2018-02-22 14:44:05 -0300

    Update production.py

--- recomendado_backend/settings/production.py
+++ recomendado_backend/settings/production.py
@@ -1,7 +1,7 @@
 from recomendado_backend.settings.base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['35.198.38.231', u'api.recomendado-dev.samir.cl', '127.0.0.1',  ]
+ALLOWED_HOSTS = ['35.199.87.113', u'api.recomendado-dev.samir.cl', '127.0.0.1',  ]
 # Here in the OS the images and files witll be uploaded and served
 MEDIA_ROOT = '/var/www/recomendado/'
 MEDIA_URL = os.path.join(BASE_DIR,'/media/')
@@ -10,9 +10,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'recomendado',
-        'HOST': '35.198.47.143',
+        'HOST': '35.192.156.205',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'Oh2fknJ1cIbAKzxv'
+        'PASSWORD': 'PC5CiouOdbcv5y53'
     }
 }
